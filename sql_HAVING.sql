8b6ce4da4b976822f869a56f6e7a6284ccf9bff9
sru0309 <srushtipatel392001@gmail.com>
2022-11-19 20:53:10 +0530

    updated

--- sql_HAVING.sql
+++ sql_HAVING.sql
@@ -1,4 +1,8 @@
 USE practice;
 SELECT * FROM employee_data;
+
 SELECT COUNT(EEID),Age FROM employee_data GROUP BY Age HAVING COUNT(EEID)>12;
+SELECT COUNT(EEID),Department FROM employee_data GROUP BY Department HAVING COUNT(EEID)>12;
+SELECT COUNT(EEID),Ethnicity FROM employee_data GROUP BY Ethnicity   HAVING COUNT(EEID)>12 ;
+
 
