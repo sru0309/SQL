638b1d85f697b430a9af76da97e9d04e7fef554a
sru0309 <srushtipatel392001@gmail.com>
2022-11-21 17:56:32 +0530

    new

---
+++ ADD_SUB.sql
@@ -0,0 +1,5 @@
+USE practice;
+SELECT (commission+salary) AS SUM FROM salesman;
+SELECT (salary-commission) AS SUM FROM salesman;
+SELECT (salary*commission) AS SUM FROM salesman;
+SELECT (salary/commission) AS SUM FROM salesman;
