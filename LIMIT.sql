53717ea42fcaebfa910d365db9300d174cc8defa
sru0309 <srushtipatel392001@gmail.com>
2022-11-22 17:43:17 +0530

    updated

---
+++ LIMIT.sql
@@ -0,0 +1,7 @@
+USE srushti;
+SELECT * FROM employee1 LIMIT 5;
+SELECT * FROM employee1 LIMIT 3;
+SELECT * FROM employee1 LIMIT 15;
+
+SELECT * FROM employee1 WHERE place='jalgoan';
+SELECT * FROM employee1 WHERE place='malkapur';
